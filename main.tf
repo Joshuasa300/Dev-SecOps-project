c808e8f959d301365ac85a6b0d028b1978dd2abc
Joshuasa300 <147273274+Joshuasa300@users.noreply.github.com>
2025-02-16 16:55:12 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
   region = "eu-north-1" # London region
 }
 resource "aws_s3_bucket" "joshuabucketforproject" {
-  bucket = "joshuabucketforproject2"
+  bucket = "joshuabucketforproject"
   }
 
 
@@ -11,7 +11,7 @@ resource "aws_s3_bucket" "joshuabucketforproject" {
 terraform {
   backend "s3" {
     #  bucket name!
-    bucket         = "joshuabucketforproject2"
+    bucket         = "joshuabucketforproject"
     key            = "global/s3/terraform.tfstate"
     region         = "eu-north-1"
   }
