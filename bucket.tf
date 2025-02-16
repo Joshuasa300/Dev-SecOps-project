1db295062933b5f9430790b7a1d0747f10f0c078
Joshuasa300 <147273274+Joshuasa300@users.noreply.github.com>
2025-02-16 15:39:15 +0000

    Delete bucket.tf

--- bucket.tf
+++
@@ -1,8 +0,0 @@
-# Define the provider
-provider "aws" {
-  region = "eu-west-2" # Replace with your preferred region
-}
-# Create an S3 bucket
-resource "aws_s3_bucket" "wedsbucket861239" {
-  bucket = "wedsbucket861239" # Replace with your desired unique bucket name
-}
