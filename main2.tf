0328be6947ca9d09f79f1d9e2549811ce15138c5
Joshuasa300 <147273274+Joshuasa300@users.noreply.github.com>
2025-02-16 15:22:37 +0000

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -1,8 +1,1 @@
-# Define the provider
-provider "aws" {
-  region = "eu-north-1" # Replace with your preferred region
-}
-# Create an S3 bucket
-resource "aws_s3_bucket" "wedsbucket" {
-  bucket = "joshuabucketforproject" # Replace with your desired unique bucket name
-}
+
