2879e429bd50b7b565c2ee76386f7533902e0e23
Joshuasa300 <147273274+Joshuasa300@users.noreply.github.com>
2025-02-17 23:33:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,14 +8,17 @@ terraform {
     key    = "global/s3/terraform.tfstate"
     region = "eu-north-1"
 
-  }
-}
-
 
 resource "aws_s3_bucket" "joshuabucketforproject" {
   bucket = "joshuabucketforproject"
   acl    = "public-read"  # This will trigger a security warning
 }
+
+
+  }
+}
+
+
 
 
 
